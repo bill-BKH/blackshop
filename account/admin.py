617aa9a1ef462225460a66b454ddc0b9a77340e5
pcsmiy34 <152594584+pcsmiy34@users.noreply.github.com>
2024-02-27 14:18:59 +0330

    widget task done extra username form created

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import User
 # Register your models here.
-admin.site.register(User)
+class useradmin(admin.ModelAdmin):
+    list_display = ['email','username']
+
+
+admin.site.register(User,useradmin)
